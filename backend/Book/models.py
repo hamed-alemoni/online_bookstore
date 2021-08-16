5da88cf39c1fe1dc601d588616f110f54e4e592d
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-16 18:28:55 +0430

    write book models

--- backend/Book/models.py
+++ backend/Book/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+# create book publisher model
+class BookPublisher(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+
+
+# create subject of book model
+class BookSubject(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=100, unique=True)
+
+
+# create type of book model
+class BookType(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=100, unique=True)
+
+
+# create book model
+class Book(models.Model):
+    # book info without relation
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authors')
+    number_of_pages = models.IntegerField(blank=False, null=False)
+    weight = models.IntegerField(blank=False, null=False)
+    released_year = models.DateField()
+    international_standard_book_number = models.IntegerField()
+
+    # book info with relation
+    book_publisher = models.ForeignKey(BookPublisher, default=None, on_delete=models.CASCADE, related_name='book_publishers')
+    book_type = models.ForeignKey(BookType, default=None, on_delete=models.CASCADE, related_name='book_types')
+    book_subject = models.ForeignKey(BookSubject, default=None, on_delete=models.CASCADE, related_name='book_subjects')
