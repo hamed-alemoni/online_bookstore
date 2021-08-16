811807cdadba2f6a11a5ad02b6776c522d410be1
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-16 18:29:27 +0430

    add book models to site admin panel

--- backend/Book/admin.py
+++ backend/Book/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Book, BookPublisher,BookSubject,BookType
 # Register your models here.
+
+# register Book module
+admin.site.register(Book)
+# register Publisher module
+admin.site.register(BookPublisher)
+# register BookSubject module
+admin.site.register(BookSubject)
+# register BookType module
+admin.site.register(BookType)
