e69a19ca7c6cf2052b38ec0fe0677920f32aa556
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-18 19:35:59 +0430

    add some method to serializer to send some extra info about publisher, author, type, subject of books

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -5,6 +5,50 @@ from django.contrib.auth import get_user_model
 
 # create a serializer for Book module
 class BookSerializer(serializers.ModelSerializer):
+    # return author's info
+    def get_author_info(self, book):
+        info = {
+            'ID': book.author.pk,
+            'Username': book.author.username
+        }
+        return info
+
+    # return book publisher's info
+    def get_book_publisher_info(self, book):
+        info = {
+            'ID': book.book_publisher.pk,
+            'title': book.book_publisher.title
+        }
+
+        return info
+
+    # return book type's info
+    def get_book_type_info(self, book):
+        info = {
+            'ID': book.book_type.pk,
+            'title': book.book_type.title
+        }
+
+        return info
+
+    # return book subject's info
+    def get_book_subject_info(self, book):
+        info = {
+            'ID': book.book_subject.pk,
+            'title': book.book_subject.title
+        }
+
+        return info
+
+    # set a method to return author's info
+    author = serializers.SerializerMethodField('get_author_info')
+    # set a method to return book publisher's info
+    book_publisher = serializers.SerializerMethodField('get_book_publisher_info')
+    # set a method to return book type's info
+    book_type = serializers.SerializerMethodField('get_book_type_info')
+    # set a method to return book subject's info
+    book_subject = serializers.SerializerMethodField('get_book_subject_info')
+
     class Meta:
         model = Book
         fields = '__all__'
