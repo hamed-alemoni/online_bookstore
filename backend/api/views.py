9feeba1fd5b9aeae3bc52bdc1c269640074dcd43
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-18 19:36:28 +0430

    add api view for booksand users

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from Book.models import Book
+from django.contrib.auth import get_user_model
+from .serializer import BookSerializer, UserSerializer
+
 
 # Create your views here.
+
+# create book view to update, delete, create and read data
+class BookViewSet(ModelViewSet):
+    # get all articles
+    queryset = Book.objects.all()
+    # determine serializer class
+    serializer_class = BookSerializer
+
+
+# create user view to read data
+class UserViewSet(ModelViewSet):
+    # get all articles
+    queryset = get_user_model().objects.all()
+    # determine serializer class
+    serializer_class = UserSerializer
