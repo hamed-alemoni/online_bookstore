6fe5fc38a5da4753e02edff2e5fb9c703fc320da
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-21 17:31:13 +0430

    create all remaining models router

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import BookViewSet, UserViewSet
+from .views import BookViewSet, UserViewSet, BookPublisherViewSet, BookTypeViewSet, BookSubjectViewSet, \
+    EducationYearViewSet
 
 app_name = 'api'
 # create a simple router
@@ -9,6 +10,18 @@ router = routers.SimpleRouter()
 # register all books urls with router
 router.register(r'books', BookViewSet, basename='books')
 
+# register all book types urls with router
+router.register(r'users', BookTypeViewSet, basename='book-types')
+
+# register all book publishers urls with router
+router.register(r'users', BookPublisherViewSet, basename='book-publishers')
+
+# register all book subjects urls with router
+router.register(r'users', BookSubjectViewSet, basename='book-subjects')
+
+# register all education year urls with router
+router.register(r'users', EducationYearViewSet, basename='education-year')
+
 # register all users urls with router
 router.register(r'users', UserViewSet, basename='users')
 
@@ -16,5 +29,4 @@ urlpatterns = [
     # set all urls that register with router
     path('', include(router.urls)),
 ]
-
 
