bdacd204777910af063f518482c6e42be90cfd3d
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-21 17:27:34 +0430

    create all view sets for models

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
-from Book.models import Book
+from Book.models import Book, BookSubject, BookType, BookPublisher, EducationYear
 from django.contrib.auth import get_user_model
-from .serializer import BookSerializer, UserSerializer
+from .serializer import BookSerializer, UserSerializer, BookSubjectSerializer, BookTypeSerializer, \
+    BookPublisherSerializer, EducationYearSerializer
 
 
 # Create your views here.
@@ -12,6 +13,38 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     # determine serializer class
     serializer_class = BookSerializer
+
+
+# create book subject view to update, delete, create and read data
+class BookSubjectViewSet(ModelViewSet):
+    # get all articles
+    queryset = BookSubject.objects.all()
+    # determine serializer class
+    serializer_class = BookSubjectSerializer
+
+
+# create book type view to update, delete, create and read data
+class BookTypeViewSet(ModelViewSet):
+    # get all articles
+    queryset = BookType.objects.all()
+    # determine serializer class
+    serializer_class = BookTypeSerializer
+
+
+# create book type view to update, delete, create and read data
+class BookPublisherViewSet(ModelViewSet):
+    # get all articles
+    queryset = BookPublisher.objects.all()
+    # determine serializer class
+    serializer_class = BookPublisherSerializer
+
+
+# create education year view to update, delete, create and read data
+class EducationYearViewSet(ModelViewSet):
+    # get all articles
+    queryset = EducationYear.objects.all()
+    # determine serializer class
+    serializer_class = EducationYearSerializer
 
 
 # create user view to read data
