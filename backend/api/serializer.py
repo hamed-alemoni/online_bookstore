b113c802f59e29df29eccf01c5f30355a78a7522
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-21 17:30:03 +0430

    add all serializers for models and add a method to book serializer to show education year detail in our way

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from Book.models import Book
+from Book.models import Book, BookSubject, BookType, BookPublisher, EducationYear
 from django.contrib.auth import get_user_model
 
 
-# create a serializer for Book module
+# create a serializer for Book model
 class BookSerializer(serializers.ModelSerializer):
     # return author's info
     def get_author_info(self, book):
@@ -40,6 +40,15 @@ class BookSerializer(serializers.ModelSerializer):
 
         return info
 
+    # return book education year's info
+    def get_book_education_year(self, book):
+        info = {
+            'ID': book.book_education_year.pk,
+            'title': book.book_education_year.title
+        }
+
+        return info
+
     # set a method to return author's info
     author = serializers.SerializerMethodField('get_author_info')
     # set a method to return book publisher's info
@@ -48,14 +57,43 @@ class BookSerializer(serializers.ModelSerializer):
     book_type = serializers.SerializerMethodField('get_book_type_info')
     # set a method to return book subject's info
     book_subject = serializers.SerializerMethodField('get_book_subject_info')
+    book_education_year = serializers.SerializerMethodField('get_book_education_year')
 
     class Meta:
         model = Book
         fields = '__all__'
 
 
-# create a serializer for User module
+# create a serializer for User model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
+        fields = '__all__'
+
+
+# create a serializer for Book Subject model
+class BookSubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookSubject
+        fields = '__all__'
+
+
+# create a serializer for Book Type model
+class BookTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookType
+        fields = '__all__'
+
+
+# create a serializer for Book Publisher model
+class BookPublisherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookPublisher
+        fields = '__all__'
+
+
+# create a serializer for Book Education Year model
+class EducationYearSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EducationYear
         fields = '__all__'
