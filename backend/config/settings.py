47307c792008a1ea92968b76e895ba7a9371bfae
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-22 17:15:18 +0430

    add default permission

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,3 +132,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    # add permissions
+    'DEFAULT_PERMISSION_CLASSES': [
+        'api.permissions.IsStaffOrReadOnly',
+    ],
+}
