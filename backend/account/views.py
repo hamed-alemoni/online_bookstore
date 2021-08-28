e11082b0fde6e37c7e7232393d7774bcaa89e748
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-28 17:44:50 +0430

    add class base and functional home page view

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from Book.models import Book
+
 
 # Create your views here.
+
+@login_required
+def home(request):
+    return render(request, 'registration/home.html')
+
+
+class BookListView(LoginRequiredMixin, ListView):
+    queryset = Book.objects.all()
+    template_name = 'registration/home.html'
