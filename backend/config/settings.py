f82b2da539a04b8123f35c52e0d26dad79935dc6
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-29 19:30:20 +0430

    add static settings to use it, add widget tweaks

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'Book.apps.BookConfig',
     'account.apps.AccountConfig',
     'api.apps.ApiConfig',
+    'widget_tweaks',
     'dj_rest_auth',
     'django.contrib.sites',
     'allauth',
@@ -127,11 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ("static",)
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
