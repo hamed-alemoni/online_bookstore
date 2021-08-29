580ba6d266354b6f1ca7511f0165e700f8e35707
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-29 19:32:23 +0430

    remove additional method

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from Book.models import Book
 
 
 # Create your views here.
-
-@login_required
-def home(request):
-    return render(request, 'registration/home.html')
 
 
 class BookListView(LoginRequiredMixin, ListView):
