6cbfb3aa8a71843128ce440ef3ef122f103d0b42
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-08-31 19:01:37 +0430

    filter released year of book in my way and add a method to show image

--- backend/Book/models.py
+++ backend/Book/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.html import format_html
+from extensions.utils import jalali_converter
 
 
 # Create your models here.
 
 # create book publisher model
+
+
 class BookPublisher(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=100, unique=True)
@@ -51,17 +55,30 @@ class Book(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authors')
     number_of_pages = models.IntegerField(blank=False, null=False)
     weight = models.IntegerField(blank=False, null=False)
-    released_year = models.DateField()
+    released_year = models.DateTimeField()
     international_standard_book_number = models.CharField(max_length=13)
     price = models.IntegerField(blank=False, null=False, default=None)
     status = models.BooleanField(default=False)
+    amount = models.IntegerField(default=False)
 
     # book info with relation
     book_publisher = models.ForeignKey(BookPublisher, default=None, on_delete=models.CASCADE,
                                        related_name='book_publishers')
     book_type = models.ForeignKey(BookType, default=None, on_delete=models.CASCADE, related_name='book_types')
     book_subject = models.ForeignKey(BookSubject, default=None, on_delete=models.CASCADE, related_name='book_subjects')
-    book_education_year = models.ForeignKey(EducationYear, default=None, on_delete=models.CASCADE, related_name='book_education_year')
+    book_education_year = models.ForeignKey(EducationYear, default=None, on_delete=models.CASCADE,
+                                            related_name='book_education_year')
 
     def __str__(self):
         return self.title
+
+    def year(self):
+        return jalali_converter(self.released_year)
+
+    year.short_description = 'سال انتشار'
+
+    def image_tag(self):
+        return format_html(f'<img width=100 height=75 styles="border-radius: 5px;" src="{self.image.url}">')
+
+    image_tag.short_description = 'عکس'
+    image_tag.allow_tags = True
