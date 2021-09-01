e6d7fb6bbba6f0fed9bcdd4209d0130cbd01783a
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-09-01 19:31:30 +0430

    create a new view to add a new book

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from Book.models import Book
 
 
@@ -10,3 +10,27 @@ from Book.models import Book
 class BookListView(LoginRequiredMixin, ListView):
     queryset = Book.objects.all()
     template_name = 'registration/home.html'
+
+
+class BookCreateView(LoginRequiredMixin, CreateView):
+    model = Book
+    fields = [
+        'title',
+        'slug',
+        'description',
+        'image',
+        'author',
+        'number_of_pages',
+        'weight',
+        'released_year',
+        'international_standard_book_number',
+        'price',
+        'status',
+        'amount',
+        'book_publisher',
+        'book_type',
+        'book_subject',
+        'book_education_year',
+
+    ]
+    template_name = 'registration/book_create_update.html'
