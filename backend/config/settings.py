74ad918ef92873fe21fa723ea692871ca6bd8eaf
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-09-01 19:32:54 +0430

    set settings to use of crispy app and determine version of bootstrap

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'api.apps.ApiConfig',
     'widget_tweaks',
+    'crispy_forms',
     'dj_rest_auth',
     'django.contrib.sites',
     'allauth',
@@ -135,6 +136,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
