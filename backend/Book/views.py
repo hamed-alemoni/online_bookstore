6ac6bc1efd1ddb7c5ce6978fb2693b8cecf7aea5
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-09-16 20:22:14 +0430

    add book list view

--- backend/Book/views.py
+++ backend/Book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+from .models import Book
+
+
 # Create your views here.
+
+class BookListView(ListView):
+    queryset = Book.objects.all()
+    template_name = 'online_bookstore/products.html'
+    context_object_name = 'books'
