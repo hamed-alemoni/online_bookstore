b4f30809158274ae93ef3e77854fec23f321b0cc
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-09-18 19:30:50 +0430

    add url for book detail view and pagination

--- backend/Book/urls.py
+++ backend/Book/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from .views import (
     BookListView,
+    BookDetailView,
 )
 app_name = 'book'
 
 urlpatterns = [
-    path('', BookListView.as_view(), name='home')
+    path('', BookListView.as_view(), name='home'),
+    path('page/<int:page>', BookListView.as_view(), name='home'),
+    path('book/<slug:slug>', BookDetailView.as_view(), name='detail'),
 ]
