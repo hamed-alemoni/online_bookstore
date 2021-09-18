24b48f2790c61343efa8ebdeeb0038f56aa051d5
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-09-18 19:30:17 +0430

    add book detail view

--- backend/Book/views.py
+++ backend/Book/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Book
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 
 class BookListView(ListView):
+    paginate_by = 12
     queryset = Book.objects.all()
     template_name = 'online_bookstore/products.html'
     context_object_name = 'books'
+
+
+class BookDetailView(DetailView):
+    queryset = Book.objects.all()
+    template_name = 'online_bookstore/product-details.html'
+
