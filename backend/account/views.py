3dd0add5ae86837b633ceefbc76dc053d68b26e7
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-09-21 21:11:15 +0430

    add two views to delete and update books

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from .mixins import SuperuserAccessMixin
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from Book.models import Book, BookPublisher, BookSubject, BookType, EducationYear
 
 
@@ -13,6 +14,36 @@ class BookListView(LoginRequiredMixin, ListView):
 
 
 class BookCreateView(LoginRequiredMixin, CreateView):
+    model = Book
+    fields = [
+        'title',
+        'slug',
+        'description',
+        'image',
+        'author',
+        'number_of_pages',
+        'weight',
+        'released_year',
+        'international_standard_book_number',
+        'price',
+        'status',
+        'amount',
+        'book_publisher',
+        'book_type',
+        'book_subject',
+        'book_education_year',
+
+    ]
+    template_name = 'registration/book_create_update.html'
+
+
+class BookDeleteView(SuperuserAccessMixin, LoginRequiredMixin, DeleteView):
+    model = Book
+    success_url = reverse_lazy('account:home')
+    template_name = 'registration/book_confirm_delete.html'
+
+
+class BookUpdateView(SuperuserAccessMixin, UpdateView):
     model = Book
     fields = [
         'title',
