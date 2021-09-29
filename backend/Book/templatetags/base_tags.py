287958f0e9e40c7a956481534d9f634ab7da046a
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-09-29 21:53:59 +0330

    add template tag folder and base tags file

---
+++ backend/Book/templatetags/base_tags.py
@@ -0,0 +1,16 @@
+from django import template
+
+# from django.contrib.contenttypes.models import ContentType
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("registration/partial/link.html")
+def link(request, link_name, content):
+    return {
+        'request': request,
+        'link_name': link_name,
+        'link': f'account:{link_name}',
+        'content': content,
+    }
