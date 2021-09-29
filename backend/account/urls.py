653b4618bf228a353f0d87ace021919540ee25b5
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-09-29 21:53:06 +0330

    uncomment logout url

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -27,8 +27,8 @@ from .views import (
 app_name = 'account'
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
-    # path('logout/', views.LogoutView.as_view(), name='logout'),
-    #
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+
     # path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
     # path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     #
