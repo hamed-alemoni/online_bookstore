181379c58ed1649eb664a948c60ff13ad10d2248
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2021-09-29 21:57:33 +0330

    add location of base_tags.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOGIN_REDIRECT_URL = 'account:home'
 LOGIN_URL = 'account:login'
+LOGOUT_REDIRECT_URL = 'account:login'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -79,6 +80,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'base_tags': 'Book.templatetags.base_tags',
+            }
         },
     },
 ]
